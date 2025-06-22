4b8996556788fd0f1b5d2f422809590d9285b778
nes <nes@fawn.zip>
2025-06-22 14:51:32 -0300

    123331

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -5,10 +5,10 @@ export const Route = createRootRoute({
   component: () => (
     <>
       <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
+        <Link to="/" className="[&.active]:font-bold [&.active]:underline">
           Home
         </Link>{' '}
-        <Link to="/about" className="[&.active]:font-bold">
+        <Link to="/about" className="[&.active]:font-bold [&.active]:underline">
           About
         </Link>
       </div>
